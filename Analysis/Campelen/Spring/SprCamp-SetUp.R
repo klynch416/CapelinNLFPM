c7c6a53595e428076e55fa4d2edd624cb39b5483
Kerri Lynch <Kerri.Lynch@mi.mun.ca>
2025-05-08 13:42:17 -0230

    Add spring Campelen models

--- Analysis/Campelen/Spring/SprCamp-SetUp.R
+++ Analysis/Campelen/Spring/SprCamp-SetUp.R
@@ -1,51 +1,74 @@
-# Set up data
-library(tidyverse)
-library(TMB)
-dyn.load("./TMBcode/SclNLFPM")
-
-
-
 # load rstrap set data
 load(file = "./Data/setdet.rda") 
 setdet <- setdet %>% mutate(v.t.s = paste(vessel,trip,set, sep = ".")) %>% 
   filter(spec == "889"|spec == "438"|spec == "892") %>%
-  mutate(lat_dd = as.numeric(format(trimws(lat.start), digits = 5)),
-         long_dd = as.numeric(format(trimws(long.start), digits = 5)))
+  mutate(lat_dd = as.numeric(format(trimws(lat.start), digits = 5)), # check that lat and longs are the same format in all data sources
+         long_dd = as.numeric(format(trimws(long.start), digits = 5))) # trimws() needed due to data entry errors
 
 
-# load and format trawl and stomach contents data
-camp_spr <- read.csv("./Data/dfo_camp_dat.csv") %>% mutate(v.t.s = paste(VESSEL,TRIP,SET, sep = ".")) %>%
-  rename(year = YEAR, trawl_pa = Capelin_PA)
+# Trawl data
+camp_spr <- read.csv("./Data/camp_capelin_2J3KL_FULL.csv") %>% rename(year = YEAR, trawl_pa = Capelin_PA) %>% 
+  mutate(v.t.s = paste(VESSEL,TRIP,SET, sep = "."),
+         season = ifelse(MONTH == 3|MONTH == 4|MONTH == 5|MONTH == 6|MONTH == 7, "spring", "spr"),
+         lat_dd = as.numeric(format(trimws(lat_dd), digits = 5)), # check that lat and longs are the same format in all data sources
+         long_dd = as.numeric(format(trimws(long_dd), digits = 5))) # trimws() needed due to data entry errors
 camp_spr <- left_join(camp_spr, 
                       setdet %>% select(v.t.s, set.depth.max, data.series, bot.temp), by = c("v.t.s"), multiple = "any")
-camp_spr <- camp_spr %>% filter(data.series == "Campelen" & season == "SPRING"& is.na(bot.temp) == FALSE) %>%
-  mutate(lat_dd = as.numeric(trimws(lat_dd)),
-         long_dd = as.numeric(trimws(long_dd)))
+camp_spr <- camp_spr %>% filter(data.series == "Campelen" & 
+                                  season == "spring"& 
+                                  is.na(bot.temp) == FALSE &
+                                  year <= 2020) 
 
 
-call_spr <- read.csv("./Data/dfo_call_dat.csv")
+# Called stomach data
+load(file = "./Data/ag.rda")
+
+call_spr <- ag %>% select(-c(source.file, oedge:gutremvol)) %>%  
+  filter((spec == "889" | spec == "438" | spec == "892") & 
+  (NAFOdiv == "2J" | NAFOdiv == "3K" |NAFOdiv == "3L") &
+  which.survey == "multispecies") %>% 
+  mutate(v.t.s = paste(vessel, trip, set, sep = "."),
+         alt.name = spec,
+         alt.name = ifelse(alt.name == "889", "American plaice", alt.name),
+         alt.name = ifelse(alt.name == "438", "Atlantic cod", alt.name),
+         alt.name = ifelse(alt.name == "892", "Greenland halibut", alt.name),
+         pa = ifelse(prey1 == "Capelin"|prey2 == "Capelin", 1, 0), 
+         content = rep("called", length(data.series)),
+         season = ifelse(month == 3|month == 4|month == 5|month == 6|month == 7, "spring", "fall")) 
 call_spr <- left_join(call_spr, 
-                      camp_spr %>% select(v.t.s, lat_dd, long_dd, set.depth.max, bot.temp), by = c("v.t.s"))
-call_spr <- call_spr %>% filter(data.series == "Campelen" & season == "spring" & is.na(bot.temp) == FALSE) %>%
-  mutate(content = rep("called", length(data.series))) %>%
-  select(year, month, content, alt.name, pa, length, bot.temp, lat_dd, long_dd)
-call_spr <- call_spr %>% filter((alt.name == "Atlantic cod" & length > 17)|(alt.name == "Greenland halibut" & length > 19)|(alt.name == "American plaice" & length > 29))
+                       setdet %>% select(v.t.s, lat_dd, long_dd, set.depth.max, bot.temp), by = c("v.t.s"), multiple = "any")
+call_spr <- call_spr %>%
+  filter(data.series == "Campelen" & 
+           season == "spring" & 
+           is.na(length) == FALSE & 
+           is.na(empty) == FALSE & 
+           is.na(bot.temp) == FALSE) %>%
+  select(year, month, season, alt.name, content, pa, empty, NAFOdiv, length, bot.temp, lat_dd, long_dd)
 
 
+
+# Full stomach data
 full_spr <- read.csv("./Data/NAFC_diet_capelin_COD_TURBOT_PLAICE_2J3KL.csv") %>% mutate(v.t.s = paste(VESSEL,TRIP,SET, sep = "."))
 full_spr <- left_join(full_spr, 
-                      camp_spr %>% select(v.t.s, data.series, set.depth.max, bot.temp), by = c("v.t.s"))
+                       setdet %>% select(v.t.s, set.depth.max, data.series, bot.temp), by = c("v.t.s"), multiple = "any")
 full_spr <- full_spr %>% mutate(alt.name = PRED_COMM_NAME,
-                                alt.name = ifelse(alt.name == "AMERICAN PLAICE", "American plaice", alt.name),
-                                alt.name = ifelse(alt.name == "COD,ATLANTIC", "Atlantic cod", alt.name),
-                                alt.name = ifelse(alt.name == "TURBOT", "Greenland halibut", alt.name),
-                                pa = ifelse(PREY_CAP == "Capelin", 1, 0),
-                                content = rep("full", length(PRED_COMM_NAME))) %>% 
-  rename(year = YEAR, month = MONTH, length = LENGTH) %>%
-  filter(data.series == "Campelen" & SEASON == "Spring" & is.na(length) == FALSE & length < 400 & is.na(bot.temp) == FALSE) %>%
-  select(year, month, content, alt.name, pa, length, bot.temp, lat_dd, long_dd)
-full_spr <- full_spr %>% 
-  filter((alt.name == "Atlantic cod" & length > 17)|(alt.name == "Greenland halibut" & length > 19)|(alt.name == "American plaice" & length > 29))
+                                  alt.name = ifelse(alt.name == "AMERICAN PLAICE", "American plaice", alt.name),
+                                  alt.name = ifelse(alt.name == "COD,ATLANTIC", "Atlantic cod", alt.name),
+                                  alt.name = ifelse(alt.name == "TURBOT", "Greenland halibut", alt.name),
+                                  pa = ifelse(PREY_CAP == "Capelin", 1, 0),
+                                  empty = ifelse(PREY_CAP == "Empty", TRUE, FALSE),
+                                  content = rep("full", length(PRED_COMM_NAME)),
+                                  SEASON = ifelse(MONTH == 3|MONTH == 4|MONTH == 5|MONTH == 6|MONTH == 7, "spring", "fall")) %>% 
+  rename(year = YEAR, month = MONTH, length = LENGTH, NAFOdiv = NAFO, season = SEASON) %>%
+  filter(data.series == "Campelen" & 
+           season == "spring" & 
+           length < 400 & 
+           is.na(length) == FALSE & 
+           is.na(bot.temp) == FALSE) %>%
+  select(year, month, season, alt.name, content, pa, empty, NAFOdiv, length, bot.temp, lat_dd, long_dd)
+
+
+
 
 # Merge species called and full stomach contents
 stom_spr <- rbind(call_spr, full_spr)
@@ -68,9 +91,9 @@ mean_catch <- terra::rast(mean_catch)
 rasValue <- raster::extract(mean_catch, cords)
 
 # Join lat & long values 
-combinePointValue <- cbind(cords, rasValue)
-# Filter good raster values
-combinePointValue <- combinePointValue %>% filter(pa > 0) ##### modify this
+combinePointValue <- cbind(cords, rasValue) %>% 
+  filter(pa > 0) # filter good raster values, mean pa in cell is be above 0
+
 
 good_coords <- data.frame("lat_dd" = combinePointValue$lat, "long_dd" = combinePointValue$long)
 # Remove coords in original df with bad pa levels
@@ -79,92 +102,31 @@ stom_spr_good <- stom_spr %>% filter(lat_dd %in% good_coords$lat_dd & long_dd %i
 
 
 # Separate data by species
-trawlf <- camp_spr_good %>% rename(pa = trawl_pa) %>% select(year, pa, bot.temp)
-AC_stof <- stom_spr_good %>% filter(alt.name == "Atlantic cod") %>% select(year, pa, content, length, bot.temp)
-GH_stof <- stom_spr_good %>% filter(alt.name == "Greenland halibut") %>% select(year, pa, content, length, bot.temp)
-AP_stof <- stom_spr_good %>% filter(alt.name == "American plaice") %>% select(year, pa, content, length, bot.temp)
+trawls <- camp_spr_good %>% rename(pa = trawl_pa) %>% select(year, pa, bot.temp) %>% mutate(geartype = "campelen")
+AC_stos <- stom_spr_good %>% filter(alt.name == "Atlantic cod") %>% select(year, pa, season, content, empty, length, bot.temp) %>% mutate(geartype = "campelen")
+GH_stos <- stom_spr_good %>% filter(alt.name == "Greenland halibut") %>% select(year, pa, season, content, empty, length, bot.temp) %>% mutate(geartype = "campelen")
+AP_stos <- stom_spr_good %>% filter(alt.name == "American plaice") %>% select(year, pa, season, content, empty, length, bot.temp) %>% mutate(geartype = "campelen")
 
 
 # Set up different length bins
-ac1 <- AC_stof %>% filter(length > 17 & length <= 45) %>% filter(year != 1998 & year != 2017)
-ac2 <- AC_stof %>% filter(length > 45) %>% filter(year != 2015)
-gh1 <- GH_stof %>% filter(length > 19 & length <= 40) %>% filter(year != 2017)
-gh2 <- GH_stof %>% filter(length > 40)
-ap1 <- AP_stof %>% filter(length > 29) %>% filter(year != 2017)
-
+ac1 <- AC_stos %>% filter(length > 17 & length <= 45) %>% filter(year != 1998 & year != 2017) # removed years with fewer than 10 overall observations
+ac2 <- AC_stos %>% filter(length > 45) %>% filter(year != 2015)
+ap1 <- AP_stos %>% filter(length > 29) %>% filter(year != 2017)
+gh1 <- GH_stos %>% filter(length > 19 & length <= 40) %>% filter(year != 2017)
+gh2 <- GH_stos %>% filter(length > 40)
 
 # Set up model env
-n <- c(length(trawlf$year),
+n <- c(length(trawls$year),
        length(ac1$year),length(ac2$year),
        length(gh1$year),length(gh2$year),
        length(ap1$year))
-pa <- c(trawlf$pa, 
+pa <- c(trawls$pa, 
         ac1$pa, ac2$pa, 
         gh1$pa, gh2$pa,
         ap1$pa)
-year <- c(trawlf$year, 
+year <- c(trawls$year, 
           ac1$year, ac2$year,
           gh1$year, gh2$year,
           ap1$year)
 names <- c("Trawl", "ac1","ac2", "gh1","gh2", "ap1")
-id <- c(seq(0,(length(names)-1)))
 
-
-sd_temp <- c(sd(trawlf$bot.temp),
-             sd(ac1$bot.temp),sd(ac2$bot.temp),
-             sd(gh1$bot.temp),sd(gh2$bot.temp),
-             sd(ap1$bot.temp))
-topt <- c(median(trawlf$bot.temp), 
-          median(ac1$bot.temp), median(ac2$bot.temp),  
-          median(gh1$bot.temp), median(gh2$bot.temp),  
-          median(ap1$bot.temp))
-temp <- c(trawlf$bot.temp, 
-          ac1$bot.temp, ac2$bot.temp,
-          gh1$bot.temp, gh2$bot.temp,
-          ap1$bot.temp)
-
-
-spp_n <- c(length(trawlf$year), 
-           sum(length(ac1$year),length(ac2$year)), 
-           sum(length(gh1$year),length(gh2$year)),
-           sum(length(ap1$year)))
-spp_id <- c(0,1,2,3)
-
-
-
-
-
-
-
-
-# 
-# table(ac1$pa, ac1$year)
-# table(ac2$pa, ac2$year)
-# table(gh1$pa, gh1$year)
-# table(gh2$pa, gh2$year)
-# table(ap1$pa, ap1$year)
-# 
-# table(ac1$content)
-# table(ac2$content)
-# table(gh1$content)
-# table(gh2$content)
-# table(ap1$content)
-# 
-# 
-# # %empty stomachs - have to change
-# length(filter(ac1, pa == 0)$pa)/length(ac1$pa)*100
-# length(filter(ac2, pa == 0)$pa)/length(ac2$pa)*100
-# length(filter(gh1, pa == 0)$pa)/length(gh1$pa)*100
-# length(filter(gh2, pa == 0)$pa)/length(gh2$pa)*100
-# length(filter(ap1, pa == 0)$pa)/length(ap1$pa)*100
-# 
-# 
-# 
-# table(AC_stof$content)
-# table(GH_stof$content)
-# table(AP_stof$content)
-# 
-# 
-# length(filter(AC_stof, pa == 0)$pa)/length(AC_stof$pa)*100
-# length(filter(GH_stof, pa == 0)$pa)/length(GH_stof$pa)*100
-# length(filter(AP_stof, pa == 0)$pa)/length(AP_stof$pa)*100
